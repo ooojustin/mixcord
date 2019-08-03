978715c633d5e7ffa7c86e0dce3d88d9733677bd
Justin <justin@garofolo.net>
2019-08-02 23:54:47 -0400

    discord bot setup (thus far)

--- mixcord/__main__.py
+++ mixcord/__main__.py
@@ -1,13 +1,26 @@
-import requests, json, sys
+import json
 from mixer import MixerAPI
 
-import database
-database.init()
-
+# determine settings
 settings_raw = open("settings.cfg").read()
 settings = json.loads(settings_raw)
 
-mixer = MixerAPI(settings["mixer"]["client-id"], settings["mixer"]["client-secret"])
-id = mixer.get_channel_id("ooojustin")
-chats = mixer.get_chats(id)
-print(json.dumps(chats))
+# database initialization
+import database
+database.init()
+
+# discord bot initialization
+import discord, logging
+from discord.ext import commands
+logging.basicConfig(level = logging.ERROR)
+bot = commands.Bot(command_prefix='!')
+
+@bot.event
+async def on_ready():
+    print('discord logged in:', bot.user)
+
+@bot.command()
+async def mixcord(ctx):
+    await ctx.send("got it")
+
+bot.run(settings["discord"]["token"])
