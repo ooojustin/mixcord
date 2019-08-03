eccd6a50fbe4eccc39650c7adeea89164b9c29d8
Justin <justin@garofolo.net>
2019-08-02 23:19:47 -0400

    database init + discord settings in template

--- mixcord/__main__.py
+++ mixcord/__main__.py
@@ -1,13 +1,13 @@
 import requests, json, sys
 from mixer import MixerAPI
 
+import database
+database.init()
+
 settings_raw = open("settings.cfg").read()
 settings = json.loads(settings_raw)
 
-# https://mixer.com/oauth/authorize?client_id=7472a113de477580849d668215f8aaaf37e0ecdea991be7f&response_type=code&redirect_uri=https:%2F%2Fjustin.ooo&scope=...
-
-# m = MixerAPI(settings["mixer-client-id"], settings["mixer-client-secret"])
-m = MixerAPI("", "")
-channel = m.get_channel("ooojustin")
-chats = m.get_chats(id)
+mixer = MixerAPI(settings["mixer"]["client-id"], settings["mixer"]["client-secret"])
+id = mixer.get_channel_id("ooojustin")
+chats = mixer.get_chats(id)
 print(json.dumps(chats))
