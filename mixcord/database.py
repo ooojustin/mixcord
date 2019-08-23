e81e919859c910a5d3f6a3844f9b8adce0b7ebcb
Justin <justin@garofolo.net>
2019-08-23 16:36:10 -0400

    improvements to database module/table structure

--- mixcord/database.py
+++ mixcord/database.py
@@ -6,10 +6,14 @@ db = None
 cursor = None
 loop = asyncio.get_event_loop()
 
+# <editor-fold> ID Types
 class IDType(Enum):
-    USER = 1
-    CHANNEL = 2
-    DISCORD = 3
+    USER = "id"
+    CHANNEL = "channel"
+    DISCORD = "discord"
+
+idtype_column = lambda x: x.value if isinstance(x, IDType) else x
+# </editor-fold>
 
 async def _init():
 
@@ -30,49 +34,40 @@ async def _init():
         return await cursor.fetchone()
 
     # create the mixcord table, if it doesnt exist
-    if not await table_exists("mixcord"):
+    if not await table_exists("users"):
         await cursor.execute("""
-        CREATE TABLE "mixcord" (
-        	"id"	INTEGER PRIMARY KEY AUTOINCREMENT,
-        	"user_id"	INTEGER UNIQUE,
-            "channel_id" INTEGER UNIQUE,
-        	"discord_id"	INTEGER UNIQUE,
-            "balance"	INTEGER DEFAULT 1000,
-        	"access_token"	TEXT,
-        	"refresh_token"	TEXT,
-        	"expires"	INTEGER DEFAULT 0
-        )
+            CREATE TABLE "users" (
+            	"id"	INTEGER NOT NULL UNIQUE,
+            	"channel"	INTEGER NOT NULL UNIQUE,
+            	"balance"	INTEGER NOT NULL DEFAULT 0,
+            	"discord"	INTEGER DEFAULT NULL,
+            	"access_token"	TEXT DEFAULT NULL,
+            	"refresh_token"	TEXT DEFAULT NULL,
+            	"expires"	INTEGER DEFAULT NULL,
+            	PRIMARY KEY("id")
+            );
         """)
 
-def column_from_type(id_type):
-    columns = {
-        IDType.USER: "user_id",
-        IDType.CHANNEL: "channel_id",
-        IDType.DISCORD: "discord_id"
-    }
-    id_type = IDType(id_type)
-    return columns.get(id_type)
-
-async def get_user(id, id_type = 1):
-    column = column_from_type(id_type)
-    query = "SELECT * FROM mixcord WHERE {} = ?".format(column)
+async def get_user(id, id_type = "id"):
+    column = idtype_column(id_type)
+    query = "SELECT * FROM users WHERE {} = ?".format(column)
     await cursor.execute(query, (id,))
     return await cursor.fetchone()
 
-async def insert_user(user_id, channel_id, discord_id):
-    query = "INSERT INTO mixcord (user_id, channel_id, discord_id) VALUES (?, ?, ?)"
+async def insert_user(user_id, channel_id, discord_id = None):
+    query = "INSERT INTO users (id, channel_id, discord_id) VALUES (?, ?, ?)"
     params = (user_id, channel_id, discord_id)
     await cursor.execute(query, params)
 
-async def update_tokens(id, access_token, refresh_token, expires, id_type = 1):
-    column = column_from_type(id_type)
-    query = "UPDATE mixcord SET access_token = ?, refresh_token = ?, expires = ? WHERE {} = ?".format(column)
+async def update_tokens(id, access_token, refresh_token, expires, id_type = "id"):
+    column = idtype_column(id_type)
+    query = "UPDATE users SET access_token = ?, refresh_token = ?, expires = ? WHERE {} = ?".format(column)
     params = (access_token, refresh_token, expires, id)
     await cursor.execute(query, params)
 
-async def add_balance(id, amount, id_type = 1):
-    column = column_from_type(id_type)
-    query = "UPDATE mixcord SET balance = balance + ? WHERE {} = ?".format(column)
+async def add_balance(id, amount, id_type = "id"):
+    column = idtype_column(id_type)
+    query = "UPDATE users SET balance = balance + ? WHERE {} = ?".format(column)
     params = (amount, id)
     await cursor.execute(query, params)
 
