1d56ff79ddd7c93860185fb875f601a83e4a0bae
Justin <justin@garofolo.net>
2019-08-23 16:14:07 -0400

    removed mixcord command (no longer needed)

--- mixcord/bots/discord.py
+++ mixcord/bots/discord.py
@@ -70,49 +70,6 @@ async def uptime(ctx):
 async def on_ready():
     print('discord logged in:', bot.user)
 
-# triggered when !mixcord command is executed in discord
-@bot.command()
-async def mixcord(ctx):
-
-    # make sure discord id isn't already in database
-    discord_id = ctx.author.id
-    if database.get_user(discord_id, database.IDType.DISCORD) is not None:
-        await ctx.author.send("You've already linked your Mixer account via mixcord.")
-        return
-
-    # get shortcode stuff from mixer
-    shortcode = await api.get_shortcode()
-    code = shortcode["code"]
-    handle = shortcode["handle"]
-
-    # tell the user what to do to link their mixer account
-    await ctx.author.send("Visit the following page to link your Mixer: <https://mixer.com/go?code={}>".format(code))
-
-    # poll shortcode checking endpoint with handle until we can move on with authorization_code
-    while True:
-        await asyncio.sleep(10)
-        response = await api.check_shortcode(handle)
-        status_code = response.status_code
-        if status_code == 200:
-            authorization_code = response.json()["code"]
-            break
-        elif status_code == 403:
-            await ctx.author.send("Failed: user denied permissions.")
-            return
-        elif status_code == 404:
-            await ctx.author.send("Failed: verification timed out.")
-            return
-
-    tokens = await api.get_token(authorization_code)
-    token_data = await api.check_token(tokens["access_token"])
-    user = await api.get_user(token_data["sub"])
-
-    database.insert_user(user.id, user.channel.id, discord_id)
-    database.update_tokens(discord_id, tokens["access_token"], tokens["refresh_token"], token_data["exp"], database.IDType.DISCORD)
-
-    await ctx.author.send("Your Mixer account has been linked: " + user.username)
-    await mixer_chat.send_message("@{} has linked their discord account: {}".format(user.username, ctx.author))
-
 async def send_announcement(message):
     guild = bot.get_guild(settings["discord"]["guild"])
     channel = discord.utils.get(guild.text_channels, name = "announcements")
